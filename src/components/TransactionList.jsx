4eb9e896abbf36cf05a62bba3197f287b5b2909d
Juan Diego Serrano <juandserrano@gmail.com>
2022-05-22 16:37:46 -0400

    Checkpoint commit

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Transaction from "./Transaction.jsx"
+import "./TransactionList.css";
 
 const TransactionList = () => {
 
   const [transactions, setTransactions] = useState([{}])
 
-  useEffect(async () => {
-    try {
-      let response = await fetch('http://localhost:3003/api/transactions');
-      let data = await response.json();
-      setTransactions(data);
-    } catch (error) {
-      console.log(error);
-      return { success: false };
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        let response = await fetch('http://localhost:3003/api/transactions');
+        let data = await response.json();
+        setTransactions(data);
+      } catch (error) {
+        console.log(error);
+        return { success: false };
+      }
     }
+    fetchData();
   }, []);
 
   return (
     <ul>
       {transactions.map(t => 
-        <li key={t.name+t.vendor}>
+        <li key={t.UUID}>
           <Transaction t={t}/>
         </li>
       )}
